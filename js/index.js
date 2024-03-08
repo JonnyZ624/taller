f004b0820c449348aa3d793f23c0a381c6098951
jonnyz624 <tapiajonathan624@gmail.com>
2024-03-08 08:59:48 -0500

    dinamismoindex

---
+++ js/index.js
@@ -0,0 +1,12 @@
+function hacerMasGrande() {
+    var encabezado = document.querySelector('.Encabezado');
+    encabezado.style.transform = 'scale(1.5)';
+}
+
+function redirigirAQuienesSomos() {
+    window.location.href = 'Nosotros.html'; // Reemplaza con la ruta correcta de tu página
+}
+
+function redirigiraqueofrecemos() {
+    window.location.href = 'Productos.html'; // Reemplaza con la ruta correcta de tu página
+}
